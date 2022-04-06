c456fae36cbecde0241f81e997cb5250c89464db
Kian McKenna <kian.p.m.007@gmail.com>
2022-04-06 10:15:43 -0500

    update: styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,9 @@
 
 <div class="container">
     <p>vote</p>
-    <Heading>CARO&shy;LINE MART&shy;UCCI</Heading>
+    <div style="text-align: center;">
+      <Heading tag="endorsement">CARO&shy;LINE MART&shy;UCCI</Heading>
+    </div>
     <p>for '23 class president</p>
     <div style="font-size: .9rem; text-align: center;">
       <Link href="/campaign/why_im_not_running">Why I dropped out of the running?</Link>
